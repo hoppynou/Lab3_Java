dd509e9a67a386e930feaae30dc48e7971ce8684
Hoppynou <ilya2007lubimov@gmail.com>
2026-03-24 21:38:07 +0600

    Setup MainActivity navigation for Lab 3 tasks

--- app/src/main/java/com/example/lab3/MainActivity.java
+++ app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.lab3;
 
+import android.content.Intent; // Обязательно добавь этот импорт
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        // Устанавливаем разметку с кнопками
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // 1. Переход на экран с LinearLayout
+        findViewById(R.id.btn_linear).setOnClickListener(v ->
+                startActivity(new Intent(this, LinearActivity.class)));
+
+        // 2. Переход на экран с ConstraintLayout
+        findViewById(R.id.btn_constraint).setOnClickListener(v ->
+                startActivity(new Intent(this, ConstraintActivity.class)));
+
+        // 3. Переход на экран с FrameLayout (пока будет гореть красным, сейчас создадим)
+        findViewById(R.id.btn_frame).setOnClickListener(v ->
+                startActivity(new Intent(this, FrameActivity.class)));
     }
 }
